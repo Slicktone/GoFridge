5d1c3c70f297d09d4fdba827f66cbf3a563652d3
TonyUCLA <TonyUCLA>
2017-01-29 23:59:14 -0800

    Route code entered, test endpoints made

--- routes/user-api-routes.js
+++ routes/user-api-routes.js
@@ -1,4 +1,4 @@
-// Requiring our models
+// Detail routes to User management
 var db = require("../models");
 // Routes
 module.exports = function (app) {
@@ -9,6 +9,24 @@ module.exports = function (app) {
 
     app.get("/userForm", function (req, res) {
         res.render("userForm");
-    })
+    });
 
+    // Creating a user
+    app.post("/userForm", function (req, res) {
+
+    })
+    // Updating a user
+    app.put("/userForm/", function(req, res) {
+        
+    })
+    // Deleting a user
+    app.delete("/userForm/:id", function(req, res) {
+        db.user.destroy({
+            where: {
+                id: req.params.id
+            }
+        }).then(function(dbUser) {
+            res.json(dbUser);
+        });
+    });
 }
