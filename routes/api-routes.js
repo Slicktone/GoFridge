db841450b5673b1b40d946be20fc47ca89137a16
Gino Park <gino.park86@gmail.com>
2017-01-29 20:45:04 -0800

    added another get route for user form

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,10 +1,14 @@
 // Requiring our models
 var db = require("../models");
 // Routes
-module.exports = function(app) {
-// created route to render index.handlebars file
-    app.get("/", function(req,res){
+module.exports = function (app) {
+    // created route to render index.handlebars file
+    app.get("/", function (req, res) {
         res.render("index");
     });
+
+    app.get("/userForm", function (req, res) {
+        res.render("userForm");
+    })
 
 }
