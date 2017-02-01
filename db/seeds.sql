6c52bae15b66108564f3f76be157ad22aeab5eb6
bryantsands <bryantsands@bryantsands.local>
2017-02-01 10:58:57 -0800

    pulled from upstream

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,11 +1,13 @@
-INSERT INTO users (name, email, password, budget) VALUES ("Omar", "omar@yahoo.com", '12345', 200);
-INSERT INTO users (name, email, password, budget) VALUES ("Tony", "tony@yahoo.com", 'password123', 200);
-INSERT INTO users (name, email, password, budget) VALUES ("Eugene", "eugene@yahoo.com", 'password', 200);
-INSERT INTO users (name, email, password, budget) VALUES ("Bryant", "bryant@yahoo.com", '789', 200);
+INSERT INTO users (first_name, last_name, email, password, budget) VALUES ("Omar", "Johnson", "omar@yahoo.com", '12345', 200);
+INSERT INTO users (first_name, last_name, email, password, budget) VALUES ("Tony", "Bobby", "tony@yahoo.com", 'password123', 200);
+INSERT INTO users (first_name, last_name, email, password, budget) VALUES ("Eugene", "Boss", "eugene@yahoo.com", 'password', 200);
+INSERT INTO users (first_name, last_name, email, password, budget) VALUES ("Bryant", "Doe", "bryant@yahoo.com", '789', 200);
 
+INSERT INTO myFridges (category, name, price, refill, userId) VALUES ("produce", "apple", 1, "2016-05-06", 1);
+INSERT INTO myFridges (category, name, price, refill, userId) VALUES ("protein", "pork", 10, "2016-12-22", 1);
+INSERT INTO myFridges (category, name, price, refill, userId) VALUES ("vegetables", "broccoli", 2, "2016-08-19", 2);
+INSERT INTO myFridges (category, name, price, refill, userId) VALUES ("snack", "chips", 4, "2017-07-06", 3);
 
-
-INSERT INTO myFridges (category, name, price, refill, userId) VALUES ("produce", "apple", 1.00, 01/15/16, 1);
-INSERT INTO myFridges (category, name, price, refill, userId) VALUES ("protein", "pork", 10.00, 01/31/16, 1);
-INSERT INTO myFridges (category, name, price, refill, userId) VALUES ("vegetables", "broccoli", 2.00, 02/01/16, 2);
-INSERT INTO myFridges (category, name, price, refill, userId) VALUES ("snack", "chips", 4.00, 01/28/17, 3);
+INSERT INTO deals (category, name, price) VALUES ("produce", "apple", 0.90);
+INSERT INTO deals (category, name, price) VALUES ("protein", "pork", 8);
+INSERT INTO deals (category, name, price) VALUES ("snack", "chips", 3);
