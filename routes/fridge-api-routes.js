6340d3ad08129014950549f0681e3ada0d3bd6dd
Omar Hadary <Omar Hadary>
2017-02-02 22:19:14 -0800

    myFridge showing items for the specific user email

--- routes/fridge-api-routes.js
+++ routes/fridge-api-routes.js
@@ -7,21 +7,40 @@ module.exports = function(app) {
 		res.render("index");
 	});
 	// GET route for all the items in myFridge
-	app.get("/member", function(req, res) {
-		var query = {};
-		if (req.query.users_id) {
-			query.userId = req.query.users_id;
-		}
-		db.myFridges.findAll({
-			where: query,
-			include: [db.users]
+	app.post("/member", function(req, res) {
+		// var query = {};
+		// if (req.query.users_id) {
+		// 	query.userId = req.query.users_id;
+		// }
+		console.log("DDDDDDD");
+		console.log(req.body);
+		console.log("DDDDDDD");
+		db.users.findOne({
+			where: {
+				email: req.body.email
+			},
+			include: [db.myFridges]
 		}).then(function(data) {
+			console.log(data);
+			console.log(data.myFridges);
 			var hbsObject = {
-				myFridges: data
+				myFridges: data.myFridges
 			};
-			console.log(hbsObject);
+			console.log("this is "+hbsObject);
 			res.render("member", hbsObject);
 		});
+			
+		
+		// db.myFridges.findAll({
+		// 	where: query,
+		// 	include: [db.users]
+		// }).then(function(data) {
+		// 	var hbsObject = {
+		// 		myFridges: data
+		// 	};
+		// 	console.log(hbsObject);
+		// 	res.render("member", hbsObject);
+		// });
 	});
 	// POST route for adding an item to myFridge
 	app.post("/member/add", function(req, res) {
