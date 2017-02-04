6f9f012d4594291f8207352d75ad8b299e128af3
TonyUCLA <TonyUCLA>
2017-02-03 17:57:34 -0800

    Omar's recent changes

--- routes/fridge-api-routes.js
+++ routes/fridge-api-routes.js
@@ -6,30 +6,68 @@ module.exports = function(app) {
 	app.get("/", function(req, res) {
 		res.render("index");
 	});
-
 	// GET route for all the items in myFridge
-	app.get("/member", function(req, res) {
-
-		var query = {};
-		if (req.query.users_id) {
-			query.userId = req.query.users_id;
-		}
-		db.myFridges.findAll({
-			where: query,
-			include: [db.users]
-
+	// app.get("/member", function(req, res) {
+	// 	res.redirect("/member");
+	// });
+	app.post("/member", function(req, res) {
+		// var query = {};
+		// if (req.query.users_id) {
+		// 	query.userId = req.query.users_id;
+		// }
+		console.log("DDDDDDD");
+		console.log(req.body);
+		console.log("DDDDDDD");
+		db.users.findOne({
+			where: {
+				email: req.body.email
+			},
+			include: [db.myFridges]
 		}).then(function(data) {
+			console.log(data);
+			console.log(data.myFridges);
 			var hbsObject = {
-				myFridges: data
+				myFridges: data.myFridges
 			};
-			console.log(hbsObject);
+			console.log("this is "+hbsObject);
 			res.render("member", hbsObject);
-
 		});
+			
+		
+		// db.myFridges.findAll({
+		// 	where: query,
+		// 	include: [db.users]
+		// }).then(function(data) {
+		// 	var hbsObject = {
+		// 		myFridges: data
+		// 	};
+		// 	console.log(hbsObject);
+		// 	res.render("member", hbsObject);
+		// });
 	});
 	// POST route for adding an item to myFridge
 	app.post("/member/add", function(req, res) {
-
+		console.log("this is the email " + req.body.email);
+		db.users.findOne({
+			where: {
+				email: req.body.email
+			}
+		}).then(function(data) {
+			if (data) {
+			
+			db.myFridges.create({				
+				category: req.body.category,
+				name: req.body.name,
+				price: req.body.price,
+				refill: req.body.refill,
+				userId: data.id
+			}).then(function() {
+				res.redirect("/member");
+			}).catch(function(error) {
+				console.log(error);
+			});
+			}
+				res.redirect("/member");
 		});
 	});
 	// PUT route for updating myFridge item refill date
@@ -47,7 +85,7 @@ module.exports = function(app) {
 		});
 	});
 	// DELETE route for deleting items from myFridge
-
+	app.delete("/member/delete/:id", function(req, res) {
 		db.myFridges.destroy({
 			where: {
 				id: req.params.id
