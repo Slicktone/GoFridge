27e6352bccb885110cff6f1565aff0be9474779b
TonyUCLA <TonyUCLA>
2017-02-03 17:57:19 -0800

    Passport Updates

--- routes/user-api-routes.js
+++ routes/user-api-routes.js
@@ -1,80 +1,20 @@
 // Detail routes to User management
-var db = require("../models");
+var db = require("../models/users.js");
 var passport = require("passport");
 
-
 // Routes
-module.exports = function(app, passport) {
-
-    // app.get('/',
-    //   function(req, res) {
-    //     res.render('index', 
-		// 			{ 
-		// 				user: req.users 
-		// 			});
-    //   });
-
-      // app.get('/member',
-      //     function(req, res) {
-      //       res.render('member', { users: req.users });
-      //     });
-
-
-
-
-      // app.get("/userForm", function(req, res) {
-      //   res.render("userForm");
-      // });
-
-      // app.post("/userForm/add", function(req, res) {
-      //     db.users.create({
-      //       email: req.body.email,
-      //       password: req.body.password
-      //     }).then(function(){
-      //       res.redirect('member');
-      //     });
-      //   });
-  // }
-
-// module.exports = function(app) {
+module.exports = function(app) {
 	// route to render userForm.handlebars file
 	app.get("/userForm", function(req, res) {
 		res.render("userForm");
 	});
-
-// Passport Login sequence
-
-	app.post('/login',
-		passport.authenticate('local', {
-			failureRedirect: 'login' 
-		}),
-		function(req, res) {
-			console.log(req.users);
-			res.render('/login', 
-				{ 
-					users: req.users
-				});
-		});
-
-    // app.get('/login',
-    //   function(req, res){
-    //     res.render('member');
-		// 		// Note: passport.authenticate() middleware invokes req.login() automatically. This function is 
-		// 		// primarily used when users sign up, during which req.login() 
-		// 		// can be invoked to automatically log in the newly registered user.
-		// 	req.login(users, function(err) {
-		// 	if (err) { return next(err); }
-		// 	return res.redirect('/users/' + req.users.email);
-		// });
-      // });
-
-
-
 	// Existing user login
 	// app.post("/login", function(req, res) {
 	// 	console.log(req.body);
 	// 	res.redirect("/member");
 	// });
+
+
 	// Show the signed in user details
 	app.get("/userForm/:id", function(req, res) {
 		db.users.findOne({
@@ -86,22 +26,84 @@ module.exports = function(app, passport) {
 			res.json(data);
 		});
 	});
+
+// Passport Authentication
+
+	//Signing Up
+	app.get("/userForm", function(req, res){
+		res.render("/userForm")
+	})
+
+	// Logging in
+	app.get("/login", function(req, res){
+		res.render("member")
+	})
+
+	// Signing out of any session
+	app.get("/signout", function(req, res){
+		req.session.destroy()
+		res.redirect("/");
+	})
+// Login Auth Strategy
+	app.post("/login", passport.authenticate("local", {
+		failureRedirect: "/login",
+		successRedirect: "/member"
+	}))
+// Signup Creating Strategy
+	app.post("/userForm/add", function(req, res, next){
+		db.User.findOne({
+			where: {
+				username: req.body.email
+			}
+		}).then(function(user){
+			if(!user){
+				db.User.create({
+					username: req.body.email,
+					password: bcrypt.hashSync(req.body.password)
+				}).then(function(user){
+					passport.authenticate("local",{
+						successRedirect: "/member",
+						failureRedirect: "/userForm"
+					})(req, res, next) // Passing the cb asynchronously 
+				})
+			} else {
+				res.send("This user exists");
+			}
+		})
+	})
+// Middleware to check for every route
+// Check if the user is logged in and use a local variable in views(handlebars variable)
+app.use(function(req, res, callback){
+	// Passports req.user stores user when authenticated
+	// Alternative to res.render
+	if(req.user){
+		res.locals.currentUser = req.user.username
+	}
+	callback();
+})
+
+
+
+
+
+
+
 	// Creating a user
-	app.post("/userForm/add", function(req, res) {
-		console.log(req.body);
-		db.users.create(req.body
-			// {
-			//     name: req.body.name,
-			//     email: req.body.email,
-			//     password: req.body.password,
-			//     budget: req.body.budget
-			// }
-		).then(function() {
-			res.redirect("/member");
-		}).catch(function(error) {
-			console.log(error);
-		});
-	});
+	// app.post("/userForm/add", function(req, res) {
+	// 	console.log(req.body);
+	// 	db.users.create(req.body
+	// 		// {
+	// 		//     name: req.body.name,
+	// 		//     email: req.body.email,
+	// 		//     password: req.body.password,
+	// 		//     budget: req.body.budget
+	// 		// }
+	// 	).then(function() {
+	// 		res.redirect("/member");
+	// 	}).catch(function(error) {
+	// 		console.log(error);
+	// 	});
+	// });
 	// Updating a user
 	app.put("/userForm/update/:id", function(req, res) {
 			var condition = "id = " + req.params.id;
@@ -116,30 +118,14 @@ module.exports = function(app, passport) {
 				res.redirect("/member");
 			});
 		})
-
-
-
-// Passport logout
-
-	app.get('/logout',
-		function(req, res){
-			req.logout();
-			res.redirect('/');
-	});
-
-
-
-
-
 		// Deleting a user
-	app.delete("/userForm/:id", function(req, res) {
-		db.users.destroy({
-			where: {
-				id: req.params.id
-			}
-		}).then(function() {
-			res.render("index");
-		});
-	});
+	// app.delete("/userForm/:id", function(req, res) {
+	// 	db.users.destroy({
+	// 		where: {
+	// 			id: req.params.id
+	// 		}
+	// 	}).then(function() {
+	// 		res.render("index");
+	// 	});
+	// });
 };
-
--- server.js
+++ server.js
@@ -1,67 +1,25 @@
 var express = require("express");
 var methodOverride = require("method-override");
 var bodyParser = require("body-parser");
-var passport = require("passport"),
-  LocalStrategy = require('passport-local').Strategy;
-var session = require("express-session");
+var passport = require("passport");
+var LocalStrategy = require('passport-local').Strategy;
+var session = require("express-session")({secret: "keyboard cat"});
+var bcrypt = require("bcrypt-nodejs");
 var path = require('path');
-
+// var User = require("./models/users")(db)
 
 var PORT = process.env.PORT || 8080;
 
 var app = express();
 
-
 // Requiring our models for syncing
 var db = require("./models");
-
-
-  passport.serializeUser(function(users, cb) {
-    console.log("serialized: " + users.id);
-    cb(null, users.id);
-  });
-
-  passport.deserializeUser(function(id, cb) {
-    db.Users.findOne({
-      where: {
-        'id': id
-      }
-    }, function (err, users) {
-      if (err) { return cb(err); }
-      console.log("deserialize: " + users);
-      cb(null, users);
-    });
-  });
-
-
-// Passport Local Strategy
-passport.use("local", new LocalStrategy({
-  usernameField: 'email'
-  },
-  function(email, password, done) {
-    db.Users.findOne({ where: {email: email}}).then(function(users) {
-      if (!users) {
-        return done(null, false);
-      } else if (password != users.password) {
-        return done(null, false);
-      } else {
-        return done(null, users);
-      }
-    }).catch(function(err){
-      return done(err);
-    });
-}))
 
 // Serve static content for the app from the "public" directory in the application directory.
 app.use(express.static(process.cwd() + "/public"));
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-
-
-// Passport session initialization
-app.use(passport.initialize())
-app.use(passport.session())
 
 // Override with POST having ?_method=DELETE
 app.use(methodOverride("_method"));
@@ -72,9 +30,53 @@ var exphbs = require("express-handlebars");
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
+
 // Import controllers and give the server access to them.
 require("./routes/fridge-api-routes.js")(app);
-require("./routes/user-api-routes.js")(app, passport);
+require("./routes/user-api-routes.js")(app);
+
+// ****************** Passport stuff here *****************************
+
+// Checking for authentication
+passport.use(new LocalStrategy(function(username, password, callback){
+  // Finding someone in the database with same username
+  db.User.findOne({where: {username: username}}).then(function(user, err){
+    if(err){
+      return callback(err)
+    }
+    if(!user){
+      return callback(null, false)
+    }
+    // if hashed password is not the same, return send them back
+    if(!bcrypt.compareSync(password, user.password)){
+      return callback(null, false)
+    }
+    return callback(null, user)
+  })
+}))
+
+passport.serializeUser(function(user, callback){
+  // referring to the user id
+  callback(null, user.id)
+})
+
+passport.deserializeUser(function(id, callback){
+  // findinf the user by id and prepping for logging out
+  db.User.findById(id).then(function(user){
+    callback(null, user)
+  })
+})
+
+app.use(passport.initialize())
+app.use(passport.session())
+
+
+
+
+// ***************************************************
+
+
+
 
 
 
